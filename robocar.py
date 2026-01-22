2b6a9cd6760609d61b17a0463a0c6535d8aeaef4
arzox35 <arzox35@gmail.com>
2026-01-22 23:19:35 +0100

    class de robocar

--- robocar.py
+++ robocar.py
@@ -3,5 +3,6 @@ class RoboCar(object):
         self.n = nom    # nom:string
         self.coo = coordonnees  # coordonnees:tuple(int, int)
         self.v = vitesse    # vitesse:int
-        self.s = sens  # orientation (nord:0, est:1, sud:2, ouest:3)
+        self.s = sens  # orientation (nord:0, nord-est:1, est:2, sud-est:3, sud:4, sud-ouest:5,
+                    #                   ouest:6, nord-ouest:7)
     
