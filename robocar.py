f27e57ae789584e78916e29c543a75fe9543a5fc
arzox35 <arzox35@gmail.com>
2026-01-23 18:54:21 +0100

    classe robocar

--- robocar.py
+++ robocar.py
@@ -6,3 +6,23 @@ class RoboCar(object):
         self.s = sens  # orientation (nord:0, nord-est:1, est:2, sud-est:3, sud:4, sud-ouest:5,
                     #                   ouest:6, nord-ouest:7)
     
+    def orientation(self)->str:
+        """
+        Renvoie une chaîne de caractère donnant la direction de la voiture
+        """
+        if self.s%8 == 0:
+            return "↑"
+        elif self.s%8 == 1:
+            return "↗"
+        elif self.s%8 == 2:
+            return "→"
+        elif self.s%8 == 3:
+            return "↘"
+        elif self.s%8 == 4:
+            return "↓"
+        elif self.s%8 == 5:
+            return "↙"
+        elif self.s%8 == 6:
+            return "←"
+        elif self.s%8 == 7:
+            return "↖"
