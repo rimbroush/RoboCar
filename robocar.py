e416fd4049ae8d8146d2df249829a8e0f00ff20e
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-10 23:18:16 +0100

    test

--- robocar.py
+++ robocar.py
@@ -1,10 +1,11 @@
 import math
 class RoboCar(object):
-    def __init__(self, nom:str, coordonnees:tuple, vitesse:int, angle:int):
+    def __init__(self, nom:str, coordonnees:tuple, vitesse:int, angle:int,rayon:int):
         self.n = nom    # nom:string
         self.coo = coordonnees  # coordonnees:tuple(int, int)
         self.v = vitesse    # vitesse:int
         self.a = angle  # angle:int [0;360]
+        self.r=rayon
     def avancer(self):
         """Avance la voiture selon son angle"""
         angle_rad = math.radians(self.a) 
