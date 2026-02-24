9468d4d86b115e3bbfe3cc7bb5fd12d1c16023a6
Rim <rimbroush@gmail.com>
2026-02-24 13:47:53 +0100

    ajouter la fonction avancer_x_metres

--- strategies.py
+++ strategies.py
@@ -1,8 +1,10 @@
 import simulation as sim
+import math
 class Deplacement:
     def __init__(self, robot, obstacles):
         self.robot = robot
         self.obstacles = obstacles
+        self.depart = None
         
     #Eviter les obstacles il faut reviser
     def eviter_obstacles(self, dt):
@@ -29,6 +31,25 @@ class Deplacement:
         """met la vitesse des roues à 0 pour arrêter le robot"""
         sim.set_vitesse_gauche(self.robot, 0)
         sim.set_vitesse_droite(self.robot, 0)
+
+    def avancer_x_metres(self, distance, dt, vitesse):
+        """Fait avancer le robot d'une distance donnée en mètres à une vitesse donnée."""
+        distance_pixels = distance * 100 # Convertir la distance en pixels
+        if self.depart is None: #Si c'est le premier appel, on memorise le point de depart
+            self.depart = self.robot.get_state()
+        #on avance tout droit
+        sim.set_vitesse_gauche(self.robot, vitesse)
+        sim.set_vitesse_droite(self.robot, vitesse)
+        #distance déjà parcourue depuis le départ
+        dx = self.robot.x - self.depart[0]
+        dy = self.robot.y - self.depart[1]
+        distance_parcourue = math.sqrt(dx**2 + dy**2)
+        #si on a atteint la distance cible, on arrete le robot
+        if distance_parcourue >= distance_pixels:
+            sim.set_vitesse_gauche(self.robot, 0)
+            sim.set_vitesse_droite(self.robot, 0)
+            self.depart = None #on reset le point de départ
+            return True #la distance a été atteinte
+        return False #le robot continue d'avancer
     
-        
-            
+
